e0a52799f3ca4ff37b71bbdcbd57b550520eaf28
Vikash Thakur <thakvikash622@gmail.com>
2024-06-02 00:11:02 +0530

    The database is working properly

--- UrlShortner/index.js
+++ UrlShortner/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 
+const { connectMongoDB } = require('./connect')
 const PORT = 8002;
+
+connectMongoDB("mongodb://127.0.0.1:27017/Urlshortner")
 
 
 
